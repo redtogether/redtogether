b61c9e0547b2773e6b2576c35d6b921cb9939eb0
Jack Willis <jack@attac.us>
2015-09-25 12:59:23 -0500

    Update seeds.rb for new relation names

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,42 +28,42 @@ u_test.subscribe(c_test)
 
 p_obsolete = Post.create!(
   channel: c_decarceration,
-  user: u_test,
+  poster: u_test,
   title: "Are Prisons Obsolete? by Angela Y. Davis",
   body: "https://web.archive.org/web/20150912064844/http://www.feministes-radicales.org/wp-content/uploads/2010/11/Angela-Davis-Are_Prisons_Obsolete.pdf"
 )
 
 p_new_jc = Post.create!(
   channel: c_decarceration,
-  user: u_test,
+  poster: u_test,
   title: "The New Jim Crow by Michelle Alexander",
   body: "https://libcom.org/library/new-jim-crow-mass-incarceration-age-colorblindness-michelle-alexander"
 )
 
 p_discipline = Post.create!(
   channel: c_decarceration,
-  user: u_test,
+  poster: u_test,
   title: "Discipline and Punish by Michel Foucault",
   body: "https://libcom.org/library/discipline-punish-birth-prison-michel-foucault"
 )
 
 p_forced = Post.create!(
   channel: c_decarceration,
-  user: u_test,
+  poster: u_test,
   title: "African Americans' Forced Labor by Heather Ann Thompson",
   body: "https://solidarity-us.org/node/2941"
 )
 
 p_cost = Post.create!(
   channel: c_decarceration,
-  user: u_test,
+  poster: u_test,
   title: "The Real Cost of Building and Financing Prisons and Jails",
   body: "http://www.realcostofprisons.org/papers-finance.html"
 )
 
 p_test = Post.create!(
   channel: c_test,
-  user: u_test,
+  poster: u_test,
   title: "Yet Another Test Post!",
   body: "https://www.loc.gov/"
 )
