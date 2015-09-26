462f744777726a2b98ebe92a85495d74b8d9a773
Jack Willis <jack@attac.us>
2015-09-25 19:53:39 -0500

    Fix schema mishap

--- db/migrate/20150925200757_add_indexes_to_existing_tables.rb
+++ db/migrate/20150925200757_add_indexes_to_existing_tables.rb
@@ -1,7 +1,6 @@
 class AddIndexesToExistingTables < ActiveRecord::Migration
   def change
     add_index :channel_subscriptions, :user_id
-    add_index :channels, expression: "LOWER(name)", name: "index_channels_on_lower_name", unique: true
     add_index :comments, :user_id
     add_index :comments, :post_id
     add_index :comments, :parent_id
--- db/migrate/20150926000825_add_default_to_post_comment_count.rb
+++ db/migrate/20150926000825_add_default_to_post_comment_count.rb
@@ -1,7 +1,5 @@
 class AddDefaultToPostCommentCount < ActiveRecord::Migration
   def change
     change_column_default :posts, :comments_count, 0
-
-    Post.where(comments_count: nil).update_all(comments_count: 0)
   end
 end
