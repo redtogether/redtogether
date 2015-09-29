f2ad81106b474419ea970b54ee8ba735ac410371
Jack Willis <jack@attac.us>
2015-09-29 11:36:09 -0500

    Don't do threaded code for now

--- app/helpers/voteable_helper.rb
+++ app/helpers/voteable_helper.rb
@@ -1,22 +1,22 @@
 module VoteableHelper
   def get_upvoted_downvoted(user, voteables)
     # The order of these queries is not important
-    db_query_threads = [Thread.new {
+    #db_query_threads = [Thread.new {
 
       @up_groups = Upvote
         .where(voteable: voteables, user: user)
         .group(:voteable_id)
         .count
 
-      }, Thread.new {
+    #  }, Thread.new {
 
       @down_groups = Downvote
         .where(voteable: voteables, user: user)
         .group(:voteable_id)
         .count
 
-    }]
-    ThreadsWait.all_waits(*db_query_threads)
+    #}]
+    #ThreadsWait.all_waits(*db_query_threads)
 
     voteables.each do |voteable|
       voteable.upvoted = @up_groups[voteable.id]
